265ed596c3c45b05edcd0abe7e9712ead713de80
Christopher Warren <chrisalmith@gmail.com>
2021-09-07 13:03:05 -0400

    deployment test

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const { graphqlHTTP } = require('express-graphql')
 const isAuth = require('./middleware/isAuth')
 
@@ -21,11 +22,20 @@ app.use(
     graphiql: true,
   }),
 )
+console.log(path.join(__dirname, './client/build//static'))
+// app.use('/client/build', express.static(path.join(__dirname, '/client/build/')))
+
+app.use(express.static('./client/build'))
+app.get('*', function (req, res) {
+  res.sendFile('index.html', {
+    root: path.join(__dirname, '/client/build/'),
+  })
+})
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen('4000')
+    app.listen('3000')
   })
   .catch((err) => {
     console.log(err)
