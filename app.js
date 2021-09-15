16e4e68c350c47922b4a5110715fa9e3acb988e2
Christopher Warren <chrisalmith@gmail.com>
2021-09-15 16:29:49 -0400

    installed dotenv to handle dev variables

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const cookieParser = require('cookie-parser')
 const path = require('path')
 const { graphqlHTTP } = require('express-graphql')
 const isAuth = require('./middleware/isAuth')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const expressPlayground = require('graphql-playground-middleware-express')
   .default
@@ -37,7 +40,7 @@ app.use(
   }),
 )
 app.get(
-  '/playground',
+  '/graphql',
   expressPlayground({
     endpoint: '/graphql',
     settings: {
@@ -47,16 +50,14 @@ app.get(
 )
 
 if (process.env.NODE_ENV === 'production') {
-  const { env } = require('./env')
   // Allows Express to serve production assets.
   app.use(express.static('client/build'))
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
-
 mongoose
-  .connect(process.env.MONGO_URI || env.MONGO_URI)
+  .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT || '4000')
   })
