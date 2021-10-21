6ca6ffcb4215ee39b337b96b44a2cc668251be70
Christopher Warren <chrisalmith@gmail.com>
2021-10-21 15:13:18 -0400

    sidebar skeleton

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -4,13 +4,15 @@ const SideBar = () => {
   const [sideToggle, setSideToggle] = useState(false)
   return (
     <div
-      className={`fixed top-0  w-20 h-20  transition-all duration-200 overflow-hidden
+      className={`fixed top-0 w-20 h-20 transition-all duration-200 overflow-hidden lg:h-screen lg:border-r lg:shadow-xl
       ${sideToggle && 'h-screen border-r shadow-xl'}`}
     >
       {/* Menu Button */}
       <button
-        className="w-20 h-20 text-gray-500  "
-        onClick={() => setSideToggle(!sideToggle)}
+        className="w-20 h-20 text-gray-500 lg:hidden"
+        onClick={() => {
+          setSideToggle(!sideToggle)
+        }}
       >
         {!sideToggle ? (
           <svg
@@ -26,21 +28,41 @@ const SideBar = () => {
             <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
           </svg>
         ) : (
-          <svg
-            className="w-12 h-12 mx-auto border-2 border-gray-400 rounded-full p-2"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <line x1="18" y1="6" x2="6" y2="18"></line>
-            <line x1="6" y1="6" x2="18" y2="18"></line>
-          </svg>
+          <div>
+            <svg
+              className="w-12 h-12 mx-auto border-2 border-gray-400 rounded-full p-2"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <line x1="18" y1="6" x2="6" y2="18"></line>
+              <line x1="6" y1="6" x2="18" y2="18"></line>
+            </svg>
+          </div>
         )}
       </button>
+      <a
+        className="hidden w-20 h-20 text-gray-500 lg:block"
+        href="http://www.google.com"
+      >
+        <svg
+          className="w-12 h-12 mx-auto border-2 border-gray-400 rounded-full p-2 mt-4"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          stroke="none"
+          fill="currentColor"
+          stroke-width="1"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
+        </svg>
+      </a>
+      <div>Content</div>
     </div>
   )
 }
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,21 +1,14 @@
 import FormButton from '../components/FormButton'
-import LoadingSpinner from '../components/LoadingSpinner'
 
-import { gql, useQuery } from '@apollo/client'
 import SideBar from '../components/SideBar'
-import { useState } from 'react'
 
 const Dashboard = ({ logout }: any) => {
-  let loading
-
   return (
     <div>
       <SideBar />
       <FormButton type="button" handleClick={logout}>
         Logout
       </FormButton>
-
-      {loading && <LoadingSpinner />}
     </div>
   )
 }
