9a96abcaa47ff98e204754549793fa80b10d7a82
Christopher Warren <chrisalmith@gmail.com>
2021-11-03 10:55:38 -0400

    implementing grid

--- client/src/pages/dashboard/Home.tsx
+++ client/src/pages/dashboard/Home.tsx
@@ -19,7 +19,18 @@ const Home = ({ userId }: any) => {
 
   return (
     <PageWrapper>
-      <h1 className="text-3xl  ">Hello, {userId}</h1>
+      <h1 className="text-4xl text-gray-700 font-medium">Hello, {userId}</h1>
+      <span className="text-gray-600 tracking-wider">
+        Welcome to your store's dashboard
+      </span>
+
+      <div className="grid grid-cols-3 gap-4 text-white">
+        <div className="bg-blue-500 h-20">Sales</div>
+        <div className="bg-blue-500">Orders</div>
+        <div className="bg-blue-500 row-span-3">Activity</div>
+        <div className="bg-blue-500 col-span-2">Fullfill, payments, stock</div>
+        <div className="bg-blue-500">item</div>
+      </div>
     </PageWrapper>
   )
 }
