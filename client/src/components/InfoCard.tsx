b6de90fd222fa0c30355c64bb2e571636f8ef227
Christopher Warren <chrisalmith@gmail.com>
2021-11-03 23:19:54 -0400

    continue styling

---
+++ client/src/components/InfoCard.tsx
@@ -0,0 +1,9 @@
+const InfoCard = (props: any) => {
+  return (
+    <div className="bg-white border border-gray-200 rounded-md p-4 relative">
+      {props.children}
+    </div>
+  )
+}
+
+export default InfoCard
--- client/src/pages/dashboard/Home.tsx
+++ client/src/pages/dashboard/Home.tsx
@@ -1,6 +1,7 @@
 import { useQuery, gql } from '@apollo/client'
 
 import PageWrapper from '../../components/PageWrapper'
+import InfoCard from '../../components/InfoCard'
 
 const Home = ({ userId }: any) => {
   const { data, loading, error } = useQuery(gql`
@@ -17,36 +18,82 @@ const Home = ({ userId }: any) => {
         hello, world
       </p> */
 
+  // <div className="grid grid-cols-10 gap-5 my-10">
+  //   <div className="col-span-4">
+  //     <InfoCard>
+  //       <h1>Sales</h1>
+  //       <span className="text-5xl">$1000</span>
+  //     </InfoCard>
+  //   </div>
+  //   <div className="bg-blue-500 col-span-4 min-h-full">Orders</div>
+  //   <div className="bg-blue-500 col-span-2 row-span-2">
+  // <ol className="con">
+  //   Activity
+  //   <li>item1</li>
+  //   <li>item1</li>
+  //   <li>item1</li>
+  //   <li>item1</li>
+  //   <li>item1</li>
+  //   <li>item1</li>
+  //   <li>item1</li>
+  //   <li>item1</li>
+  //   <li>item1</li>
+  //   <li>item1</li>
+  // </ol>
+  //   </div>
+  //   <div className="bg-blue-500 col-span-8">Fullfill, payments, stock</div>
+  //   <div className="bg-blue-500 col-span-8">item</div>
+  // </div>
+
   return (
     <PageWrapper>
       <h1 className="text-4xl text-gray-700 font-medium">Hello, {userId}</h1>
       <span className="text-gray-600 tracking-wider">
         Welcome to your store's dashboard
       </span>
+      {/* TODO: 
+      [] Fix page wrapped on smaller devices 
+      [] 
+      
+      */}
 
-      <div className="grid grid-cols-12 gap-5 text-white my-10">
-        <div className="bg-blue-500 col-span-4">Sales</div>
-        <div className="bg-blue-500 col-span-4">Orders</div>
-        <div className="bg-blue-500 col-span-3 row-span-2">
-          Activity
-          <ol>
-            <li>item1</li>
-            <li>item1</li>
-            <li>item1</li>
-            <li>item1</li>
-            <li>item1</li>
-            <li>item1</li>
-            <li>item1</li>
-            <li>item1</li>
-            <li>item1</li>
-            <li>item1</li>
-            <li>item1</li>
-          </ol>
+      <div className="grid grid-cols-12 md:grid-cols-1 gap-4 my-10 text-gray-800">
+        <div className="col-span-9 grid grid-cols-2 gap-4">
+          <div>
+            <InfoCard>
+              <h1 className="text-2xl">Sales</h1>
+              <span className="text-sm block">Today</span>
+
+              <span className="text-4xl block text-right">$1000.42</span>
+            </InfoCard>
+          </div>
+
+          <div>
+            <InfoCard>
+              <h1 className="text-2xl">Sales</h1>
+              <span className="text-sm block">Today</span>
+
+              <span className="text-4xl block text-right">$1000.42</span>
+            </InfoCard>
+          </div>
         </div>
-        <div className="bg-blue-500 col-span-8 h-20">
-          Fullfill, payments, stock
+
+        <div className="col-span-3">
+          <InfoCard>
+            <h1 className="text-2xl pb-4 relative">Activity</h1>
+            <div className="bg-gray-200 absolute h-px  left-0 w-full"></div>
+            <ol className="text-lg py-4">
+              <li>Order #109582 Processed</li>
+              <li>Order #109582 Processed</li>
+              <li>Order #109582 Processed</li>
+              <li>Order #109582 Processed</li>
+              <li>Order #109582 Processed</li>
+              <li>Order #109582 Processed</li>
+              <li>Order #109582 Processed</li>
+              <li>Order #109582 Processed</li>
+            </ol>
+          </InfoCard>
         </div>
-        <div className="bg-blue-500 col-span-8">item</div>
       </div>
     </PageWrapper>
   )
