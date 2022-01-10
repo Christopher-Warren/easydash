c1c1c75c551affee4a4dc026e4c1e0b797c94a89
Christopher Warren <chrisalmith@gmail.com>
2022-01-10 16:54:57 -0500

    styling

--- client/src/pages/dashboard/Products.tsx
+++ client/src/pages/dashboard/Products.tsx
@@ -31,6 +31,114 @@ const Products = ({ userId }: any) => {
   `)
   const dispatch = useAppDispatch()
 
+  const renderTable = () => {
+    if (loading) {
+      let skeleton = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+      return skeleton.map((item: any, index: any) => {
+        return (
+          <tr className="relative pointer-events-none animate-pulse">
+            <td className="px-5 py-3 w-0">
+              <input type="checkbox" className=""></input>
+            </td>
+
+            <td className="w-16 text-gray-300">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="ml-2 w-7"
+              >
+                <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+                <circle cx="8.5" cy="8.5" r="1.5"></circle>
+                <polyline points="21 15 16 10 5 21"></polyline>
+              </svg>
+            </td>
+
+            <td className=" ">
+              <div className="border-t border-gray-200 w-screen  absolute left-0 top-0 " />
+              <div className="relative py-2">
+                <div className="">
+                  <div className="text-lg  leading-none bg-gray-300 rounded-lg w-64 h-4 mb-2"></div>
+                  <div className="bg-gray-300 rounded-lg w-1/4 h-4"></div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <div className=" relative bg-gray-300 rounded-lg w-1/4 h-4"></div>
+            </td>
+            <td>
+              <div className=" relative bg-gray-300 rounded-lg w-1/4 h-4"></div>
+            </td>
+            <td className="text-right pr-5">
+              <div className=" relative bg-gray-300 rounded-lg h-4"></div>
+            </td>
+          </tr>
+        )
+      })
+    } else if (!loading && !error) {
+      return data.products.map((item: any, index: any) => {
+        return (
+          <tr className="relative hover:bg-purple-200 ">
+            <td className="px-5 py-3 w-0">
+              <input type="checkbox" className=""></input>
+            </td>
+
+            <td className="w-16 text-gray-700">
+              {item.images[0] ? (
+                <img
+                  className="w-11 h-11 object-cover object-center font-light rounded "
+                  src={item.images[0]}
+                  alt={item.name + ' '}
+                ></img>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="ml-2 w-7"
+                >
+                  <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+                  <circle cx="8.5" cy="8.5" r="1.5"></circle>
+                  <polyline points="21 15 16 10 5 21"></polyline>
+                </svg>
+              )}
+            </td>
+
+            <td>
+              <div className="border-t border-gray-200 w-screen  absolute left-0 top-0 " />
+              <div className="relative py-2">
+                <div className="w-64">
+                  <h2 className="text-lg  leading-none">{item.name}</h2>
+                  <span className="text-sm font text-gray-700 ">
+                    {item.category.name}
+                  </span>
+                </div>
+              </div>
+            </td>
+            <td>
+              <div className=" relative">{item.subcategory.name}</div>
+            </td>
+            <td>
+              <div className=" relative">{item.stock}</div>
+            </td>
+            <td className="text-right pr-5">
+              <div className=" relative">{item.price}</div>
+            </td>
+          </tr>
+        )
+      })
+    }
+  }
+
   return (
     <PageWrapper>
       <h1 className="text-4xl text-gray-700 font-medium ">Products</h1>
@@ -53,7 +161,7 @@ const Products = ({ userId }: any) => {
       </div>
 
       <TableCard>
-        <table className="w-full">
+        <table className="w-full ">
           <thead>
             <tr className="text-gray-800 text-xl">
               <th className="px-5 py-3">
@@ -63,52 +171,10 @@ const Products = ({ userId }: any) => {
               <th>Name</th>
               <th>Category</th>
               <th>Stock</th>
-              <th className="pr-3">Price</th>
+              <th className="pr-5 w-0">Price</th>
             </tr>
           </thead>
-          <tbody className="text-gray-800">
-            {!loading &&
-              !error &&
-              data.products.map((item: any, index: any) => {
-                console.log(item)
-                return (
-                  <tr className="relative hover:bg-purple-200 ">
-                    <td className="px-5 py-3 w-0">
-                      <input type="checkbox" className=""></input>
-                    </td>
-
-                    <td className="w-16">
-                      <img
-                        className="w-12 h-12 object-cover object-center font-light"
-                        src={item.images[0] ? item.images[0] : ''}
-                        alt={item.name + ' '}
-                      ></img>
-                    </td>
-
-                    <td className="">
-                      <div className="border-t border-gray-200 w-screen  absolute left-0 top-0 " />
-                      <div className="relative py-2">
-                        <div>
-                          <h2 className="text-lg  leading-none">{item.name}</h2>
-                          <span className="text-sm font text-gray-700 ">
-                            {item.category.name}
-                          </span>
-                        </div>
-                      </div>
-                    </td>
-                    <td>
-                      <div className=" relative">{item.subcategory.name}</div>
-                    </td>
-                    <td>
-                      <div className=" relative">{item.stock}</div>
-                    </td>
-                    <td className="">
-                      <div className=" relative">{item.price}</div>
-                    </td>
-                  </tr>
-                )
-              })}
-          </tbody>
+          <tbody className="text-gray-800 ">{renderTable()}</tbody>
         </table>
 
         {/* <h1 className="text-2xl relative p-3">All Products</h1>
