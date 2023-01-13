c41c51e5df523344f76e3d344d294e58e3cd063b
Christopher Warren <chrisalmith@gmail.com>
2023-01-12 22:06:08 -0500

    only render navbar in dashboard

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,18 @@ import { Provider } from "react-redux";
 import "../styles/globals.css";
 import { store } from "../redux/store";
 import SideBar from "../client/src/components/SideBar";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
   // console.log("client: ", client);
+
+  const router = useRouter();
+  const inDashboard = router.pathname.includes("/dashboard");
+
   return (
     <ApolloProvider client={client as any}>
       <Provider store={store}>
-        <SideBar />
+        {inDashboard && <SideBar />}
         <Component {...pageProps} />
       </Provider>
     </ApolloProvider>
