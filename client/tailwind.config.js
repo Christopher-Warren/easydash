56740056062daf858207f109fb567fdb2adb9012
Christopher Warren <chrisalmith@gmail.com>
2023-02-12 09:56:38 -0500

    remove old client

--- client/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
-  plugins: [
-    require("tailwindcss-children"),
-    require("@tailwindcss/aspect-ratio"),
-  ],
-  darkMode: "class",
-};
